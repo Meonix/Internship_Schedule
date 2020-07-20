42bc1921608415b57be4687725bd27742a5950b5
Mionix <duymark42@gmail.com>
2020-07-20 10:14:55 +0700

    Adding Login Activity and Navigation Fragment

---
+++ app/src/main/java/com/mionix/baseapp/ui/activity/LoginActivity.kt
@@ -0,0 +1,85 @@
+package com.mionix.baseapp.ui
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.iid.FirebaseInstanceId
+import com.mionix.baseapp.MainActivity
+import com.mionix.baseapp.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var usersRef: DatabaseReference = FirebaseDatabase.getInstance().reference.child("Users")
+
+    private lateinit var loadingBar: ProgressDialog
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setUpView()
+        handleOnclick()
+    }
+
+    private fun handleOnclick() {
+        loginButton.setOnClickListener {  allowUserToLogin()  }
+    }
+
+    private fun allowUserToLogin() {
+        val email = loginEmail.text.toString()
+        val password = loginPassword.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email....", Toast.LENGTH_SHORT).show()
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password....", Toast.LENGTH_SHORT).show()
+        } else {
+            loadingBar.show()
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val currentUserID = mAuth.currentUser!!.uid
+                    val deviceToken = FirebaseInstanceId.getInstance().token
+
+                    usersRef.child(currentUserID).child("device_token")
+                        .setValue(deviceToken)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                sendUserToMainActivity()
+                                Toast.makeText(this@LoginActivity, "Logged in  Successful....", Toast.LENGTH_SHORT).show()
+                                loadingBar.dismiss()
+                            }
+                        }
+                } else {
+                    val message = task.exception!!.toString()
+                    Toast.makeText(this@LoginActivity, "Error :$message", Toast.LENGTH_SHORT).show()
+                    loadingBar.dismiss()
+                }
+            }
+        }
+    }
+
+    private fun sendUserToMainActivity() {
+        val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+        startActivity(mainIntent)
+        finish()
+    }
+
+    private fun setUpView() {
+        loadingBar= ProgressDialog(this)
+        loadingBar.setTitle("Sign In")
+        loadingBar.setMessage("Please wait....")
+        loadingBar.setCanceledOnTouchOutside(true)
+    }
+//    private fun sendUserToRegisterActivity() {
+//        val registerIntent = Intent(this@LoginActivity, RegisterActivity::class.java)
+//        startActivity(registerIntent)
+//    }
+}
